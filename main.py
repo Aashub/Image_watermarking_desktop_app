8c7956d957ab887fc35dc7c87d38cc9f786f3780
Ashish Bamniya <ashishbam07@gmail.com>
2026-07-29 19:14:04 +0530

    edit watermark screen create, open file exp. method add, display image on screen add

--- main.py
+++ main.py
@@ -6,17 +6,19 @@
 # cloud sync to upload the watermarked image and watermark style and position to the cloud storage
 
 
-from tkinter import Tk, PhotoImage, Label, Canvas, Button
+from tkinter import Tk, PhotoImage, Label, Canvas, Button, filedialog
+from PIL import Image, ImageTk
 
 HEADING_TEXT_COLOR = "black"
 WINDOW_WIDTH = 1080
 WINDOW_HEIGHT = 700
+FILE_TYPES = [ ("Image files", "*.jpg *.jpeg *.png *.bmp *.gif"), ("All files", "*.*")]  # Define allowed image file types
+
 
 class UserInterface(Tk):
 
     def __init__(self):
         super().__init__()
-
 
 
         # this will set the game window center to the screen.
@@ -33,6 +35,7 @@ class UserInterface(Tk):
 
 
     def add_home_screen_image(self):
+        """this method add background image in home screen"""
 
         # add home screen image
         img = PhotoImage(file="img/watermark_app_home_menu_bg_img.png")
@@ -44,6 +47,7 @@ class UserInterface(Tk):
 
 
     def home_screen_heading_text(self):
+        """this method add heading text in home screen"""
 
         # home screen text canvas
         text_canvas = Canvas(width=490, height=80, bg = "#0D9BC7", highlightthickness = 0)
@@ -60,16 +64,57 @@ class UserInterface(Tk):
         self.home_screen_button()
 
     def home_screen_button(self):
+        """this method add button in home screen"""
 
         # single image button
         single_image_button = Button(self, text="Start Make Watermark", font=("Arial", 12, "bold"), fg="white",
-                                     command=lambda: print("Clicked!"), bd=0, bg="#F05A5A", height= 2)
+                                     command=self.display_watermarking_screen, bd=0, bg="#F05A5A", height= 2)
         single_image_button.place(x=355, y=300)
 
         # batch image button
         batch_image_button = Button(self, text="Start Batch Watermark", font=("Arial", 12, "bold"), fg="white",
                                     command=lambda: print("Clicked!"), bd=0, bg="#F05A5A", height= 2)
         batch_image_button.place(x=560, y=300)
+
+
+    def display_watermarking_screen(self):
+        """this method will load the screen where image is being watermarked."""
+
+        [widget.destroy() for widget in self.winfo_children()]   # this for loop destroy the previous screen all widget
+
+        # create canvas for image area
+        self.image_area_canvas = Canvas(width= 800, height=700, bg = "#313131", highlightthickness = 0)
+        self.image_area_canvas.place(x = 0, y = 0)
+
+        # create canvas for editing widget buttons
+        self.image_edit_widget_canvas = Canvas(width=280, height=700, bg="#494949", highlightthickness=0)
+        self.image_edit_widget_canvas.place(x = 800, y = 0)
+
+        # creating add image button
+        add_image_button = Button(self.image_edit_widget_canvas, text="Select Image", font=("Arial", 12, "bold"), fg="white",
+                                     command=self.open_file_explorer, bd=0, bg="#007aff", width=18,height= 2)
+        add_image_button.place(x = 48, y = 600)
+
+
+    def open_file_explorer(self):
+        """this method will open the file explorer to select the image on which user wants to add watermark."""
+
+        # Open the file dialog and get the selected file path
+        file_path = filedialog.askopenfilename(initialdir="/", title="Select Image", filetypes=(FILE_TYPES))
+
+        if file_path:
+            self.display_image(file_path)
+
+
+    def display_image(self, image_file):
+        """this method will display image on the screen"""
+
+        print(f"Selected Image: {image_file}")
+
+        # it will show image which needs to be watermarked screen image
+        self.watermarking_image = ImageTk.PhotoImage(file=image_file)
+        self.image_area_canvas.create_image(0, 0,image=self.watermarking_image, anchor="center")
+
 
 app_window = UserInterface()
 
